bf662bd30d4d16535a71ccb5c353497fcee50b2a
[lilululu] <[2841502399@qq.com]>
2018-06-14 14:46:46 +0800

    first

---
+++ src/animal/dog.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 6/14/18
+ * Time: 2:21 PM
+ */
+
+namespace src\animal;
+
+class dog{
+
+    function say(){
+
+        echo "hello, i am a dog, do u want play with me";
+    }
+
+    function run(){
+
+        echo "i can run faster and faster";
+    }
+}
